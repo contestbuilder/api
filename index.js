a75f0ce96b8adcc0fc6fa8b011a5f690b7dd0d55
Cristhian Bonilha <cristhian.bonilha@hotmail.com>
2017-07-08 14:38:37 -0300

    added morgan to log requests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express   = require('express'),
 	wagner    = require('wagner-core'),
+	morgan    = require('morgan'),
 	configLib = require('./libraries/config.lib');
 
 require('./models')(configLib.env.DATABASE_PATH);
@@ -10,6 +11,7 @@ require('./services')(wagner);
 var app = express();
 var baseUrl = '/api/v1';
 
+app.use(morgan('tiny'));
 app.use(require('./middlewares')(wagner, baseUrl));
 app.use(baseUrl, require('./controllers'));
 
